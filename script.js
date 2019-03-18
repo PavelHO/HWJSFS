a7803a0348aa0187e414d80f81008e0833e442fe
Pavel <Pavel@gmail.com>
2019-03-18 23:14:40 +0200

    hwjs1

---
+++ script.js
@@ -0,0 +1,15 @@
+var comingItems = +prompt('Комплектующие пришедшие на контроль'); //Комплектующие пришедшие на контроль
+var procDefectItems;     //Процент комплектующих с выявлеными несоответствиями
+
+do{                                                   
+	var defectItems = prompt("Ввидите количество комплектующих с несоответствиями в шт."); 
+	procDefectItems     = (defectItems / comingItems)*100;                //Определяем процент комплектующих с выявлеными несоответствиями
+	alert("% " + procDefectItems"%");                           
+}while(uahValue !== null)                          
+
+
+
+
+
+
+
